5de6a738202ae9ed95faf7f708d76f36265d71b0
Luke Schnetlage <89532838+Luke-Schnetlage@users.noreply.github.com>
2022-09-28 20:54:33 +0000

    Implemented msg_queue.c

--- .ccls-cache/@home@runner@SchnetlageLukeAssign3/msg_queue_tester.c
+++ .ccls-cache/@home@runner@SchnetlageLukeAssign3/msg_queue_tester.c
@@ -1,4 +1,4 @@
-/* Nicholas Coleman
+/* Luke Schnetlage
  * CSCI 4100 Assignment 3
  * This program tests a message queue by creating multiple sending and 
  * receiving threads
@@ -23,6 +23,14 @@ void * send_msgs(void * args);
 void * receive_msgs(void * args);
 
 /* Main function */
+/*
+1. Declare a pthread_t variable for each of the sending and receiving threads.
+2. Declare a msg_queue structure and initialize it using mq_init.
+3. Declare a thread_args structure for each of the sending and receiving threads and initialize them using thread_args_init.
+4. Start each of the sending threads using pthread_create, the send_msgs functions, and the appropriate thread_args structure.
+5. Start each of the receiving threads using pthread_create, the receive_msgs function, and the appropriate thread_args structure.
+6. Wait for each of the sending and receiving threads to complete using pthread_join.
+*/
 int main(int argc, char *argv[]) {
   /* YOUR CODE HERE */
 }
@@ -33,12 +41,31 @@ void thread_args_init(struct thread_args *args, int t_id, int num_msgs,
   /* YOUR CODE HERE */
 }
 
+
 /* Function for sending thread to execute */
+/*
+1.Cast the void pointer args to a pointer to a thread_args structure using the following statement:
+  struct thread_args * real_args = (struct thread_args *) args;
+2. Declare a mq_msg structure.
+3. Send the number of messages specified in the thread_args structure by repeatedly doing the following:
+  a. Initialize the mq_msg structure with the appropriate thread id and message id.
+  b. Send the message to the queue in the thread_args structure using mq_send.
+4. Return NULL.
+*/
 void * send_msgs(void * args) {
   /* YOUR CODE HERE */
 }
 
+
 /* Function for receiving thread to execute */
+/*
+1. Cast the void pointer args to a pointer to a thread_args structure.
+2. Declare a mq_msg structure.
+3. Receive the number of messages specified in the thread_args structure by repeatedly doing the following:
+  a. Receive the message from the queue in the thread_args structure using mq_receive.
+  b. Display the message along with the receiver thread id from the thread_args structure.
+4. Return NULL
+*/
 void * receive_msgs(void * args) {
   /* YOUR CODE HERE */
 }
--- msg_queue.c
+++ msg_queue.c
@@ -5,9 +5,19 @@
 
 #include "msg_queue.h"
 #include <pthread.h>
+
+/*
+The in and out variables are initialized to zero.
+See the section on POSIX Threads, Locks, and Condition Variables for how to initialize the lock and condition variables.
+*/
 /* Initialize the message queue */
 void mq_init(struct msg_queue * mq) {
-  /* YOUR CODE HERE */
+  mq->in = 0;
+  mq->out = 0;
+  
+  pthread_mutex_init(&mq->lock, NULL);
+  pthread_cond_init(&mq->msg_added, NULL);
+  pthread_cond_init(&mq->msg_removed, NULL);
 }
 
 /*
@@ -20,7 +30,19 @@ Release the queue lock.
 */
 /* Send a message to a message queue */
 void mq_send(struct msg_queue * mq, struct mq_msg * message) {
-  /* YOUR CODE HERE */
+  
+  pthread_mutex_lock(&mq->lock); //Acquire the queue lock
+
+  if ((&mq->in - &mq->out) > 10){ //While the queue is full, wait on the queue’s msg_removed condition variable
+    pthread_cond_wait(&mq->msg_removed, &mq->lock);
+  }
+
+  //Copy the message from the mq_msg structure provided to the correct location in the queue.
+  mq->messages[(&mq->in - &mq->out)%10] = *message;
+  
+  mq->in++; //Increment the queue’s in member variable.
+  pthread_cond_signal(&mq->msg_added);//Signal the queue’s msg_added condition variable.
+  pthread_mutex_unlock(&mq->lock);//Release the queue lock.
 }
 
 /*
@@ -33,6 +55,19 @@ Release the queue lock.
 */
 /* Receive a message from a message queue */
 void mq_receive(struct msg_queue *mq, struct mq_msg * message) {
-  /* YOUR CODE HERE */
+  pthread_mutex_lock(&mq->lock); //Acquire the queue lock
+
+  //While the queue is empty, wait on the queue’s msg_added condition variable
+  if ((&mq->in - &mq->out) < 1){ 
+    pthread_cond_wait(&mq->msg_added, &mq->lock);
+  }
+  
+  
+  //Copy the message from the correct location in the queue to the mq_msg structure provided.
+  *message = mq->messages[(&mq->in - &mq->out)%10];
+  
+  mq->out++;//Increment the queue’s out member variable.
+  pthread_cond_signal(&mq->msg_removed);//Signal the queue’s msg_removed condition variable.
+  pthread_mutex_unlock(&mq->lock);//Release the queue lock.
 }
 
